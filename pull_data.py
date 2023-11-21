7f136f123b29227ce33c77d89c58af75dec3da88
MARTIN Charles <Charles.Martin@bordeaux-inp.fr>
2023-11-21 13:34:38 +0100

    quick fix

--- pull_data.py
+++ pull_data.py
@@ -10,6 +10,6 @@ key = str(f.read())
 URL = "https://data.bordeaux-metropole.fr/geojson?key=" + key + "&typename=ci_vcub_p"
 response = requests.get(URL)
 
-https://data.bordeaux-metropole.fr/geojson/aggregate/ST_PARK_P?key=[VOTRECLE]&filter={"ident":"CUBPK42"}&rangeStart=2023-11-19T00:00:00&rangeEnd=2023-11-20T00:00:00&rangeStep=hour&attributes={"libres":"average","total":"max"}
+#https://data.bordeaux-metropole.fr/geojson/aggregate/ST_PARK_P?key=[VOTRECLE]&filter={"ident":"CUBPK42"}&rangeStart=2023-11-19T00:00:00&rangeEnd=2023-11-20T00:00:00&rangeStep=hour&attributes={"libres":"average","total":"max"}
 
 open(DIRECTORY + "dataset.json", "wb").write(response.content)
