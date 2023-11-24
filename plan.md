4dec9e0901b9bdd9b4a918c6a4c5ac5a39ee272d
Angel Hippolyte <angel.hippolyte@bordeaux-inp.fr>
2023-11-24 16:50:03 +0100

    API

--- plan.md
+++ plan.md
@@ -1,4 +1,16 @@
+# Version préliminaire : Visualisation
+
+- Affichage de quelques courbes sur une journée pour regarder les tendances sur une station
+
+
+# Regression
+
 Détécter les anomalies concernant l'occupation (vélos présents) dans une station V³ au cours d'une heure donnée en se basant sur:
 - L'occupation pécédemment relevée au cours de la même heure durant le même jour de la semaine et même mois des années précédentes
 - L'occupation relevée lors des mêmes types de jour (ouvrés, fériés, ...)
-- 
+
+Y = $\beta_0 + \beta_1 X_1 + \beta_2 X_2 + \beta_2 X_2$
+
+$X_1 = avg\_hour$\
+$X_2 = avg\_daytype$\
+$X_3 = avg\_$\
--- pull_data.py
+++ pull_data.py
@@ -6,8 +6,18 @@ DIRECTORY = "data/"
 
 f = open("key.txt", "r")
 key = str(f.read())
-# print(key,end='')
-URL = "https://data.bordeaux-metropole.fr/geojson?key=" + key + "&typename=ci_vcub_p&rangeStart=2023-11-10T00:00:00&rangeEnd=2023-11-20T00:00:00"
+parameters = {
+    "startDate": "2023-11-06", 
+    "endDate": "2023-11-07",
+    "step": "hour", #5min 15min 30min hour day week month
+    "stations": [""],
+    "categories": [""],
+}
+StartDate = ""
+URL = "https://data.bordeaux-metropole.fr/geojson/aggregate/ci_vcub_p?key=" + key +\
+      "&rangeStart=" + parameters["startDate"] + "&rangeEnd=" + parameters["endDate"] + "&rangeStep="+ parameters["step"] + \
+      "&group=time%2Bgid"
+
 response = requests.get(URL)
 
 if response.status_code == 200:
