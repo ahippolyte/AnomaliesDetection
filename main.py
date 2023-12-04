aeb4df4e228c240de78e28f12e08b9a50695ec58
MARTIN Charles <Charles.Martin@bordeaux-inp.fr>
2023-12-04 15:55:46 +0100

    affichage des annomalies

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 """ SETTINGS """
 DIRECTORY = "data/"
 DATASET = "dataset.json"
+DECONNECTEE_VALUE = "DECONNECTEE"
 
 """ PARSE """
 # Initialiser une liste pour stocker les données
@@ -25,12 +26,13 @@ with open(DIRECTORY + DATASET, "r") as read_file:
         nbvelos = feature['properties']['nbvelos']
         nbelec = feature['properties']['nbelec']
         nbclassiq = feature['properties']['nbclassiq']
+        etat = feature['properties']['etat']
 
         # Calculer le pourcentage d'occupation
-        occupation_percentage = 1 - nbplaces / (nbplaces + nbvelos )
+        occupation_percentage = 1 - nbplaces / (nbplaces + nbvelos)
 
         # Ajouter les données à la liste
-        data_list.append({'mdate': mdate, 'occupation_percentage': occupation_percentage})
+        data_list.append({'mdate': mdate, 'occupation_percentage': occupation_percentage, 'etat': etat})
 
 # Créer un DataFrame Pandas à partir de la liste de données
 df = pd.DataFrame(data_list)
@@ -41,9 +43,18 @@ df['mdate'] = pd.to_datetime(df['mdate'])
 # Trier le DataFrame par date
 df = df.sort_values(by='mdate')
 
-# Tracer la courbe d'occupation par rapport au temps
+# Tacer les points avec etat=="DECONNECTEE" en rouge et les autres en bleu
 plt.figure(figsize=(10, 6))
-plt.plot(df['mdate'], df['occupation_percentage'], label='Occupation Percentage')
+
+# Plotting points with etat=="DECONNECTEE" in red (case-insensitive) and smaller points
+plt.scatter(df[df['etat'].str.lower() == DECONNECTEE_VALUE.lower()]['mdate'], df[df['etat'].str.lower() == DECONNECTEE_VALUE.lower()]['occupation_percentage'], c='red', label=DECONNECTEE_VALUE, s=40)
+
+# Plotting other points in blue and smaller points
+plt.scatter(df[df['etat'].str.lower() != DECONNECTEE_VALUE.lower()]['mdate'], df[df['etat'].str.lower() != DECONNECTEE_VALUE.lower()]['occupation_percentage'], c='blue', label='Occupation Percentage', s=4)
+
+# Connecting all points with a line
+plt.plot(df['mdate'], df['occupation_percentage'], color='black', linestyle='-', linewidth=0.5)
+
 plt.title('Occupation Percentage Over Time')
 plt.xlabel('Date and Time')
 plt.ylabel('Occupation Percentage')
