014a1303a9f9f648a809c5177c0a9062dcc1630b
Jules EVANS <julesevans9@gmail.com>
2024-01-13 09:29:40 +0100

    added extra stuff for prophet

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ DECONNECTEE_VALUE = "DECONNECTEE"
 # Initialiser une liste pour stocker les données
 data_list = []
 data_list_per_day = []
+data_list_prophet = []
 
 """ PROPHET INSTANCE"""
 prophet_instance = pr.Prophet()
@@ -56,41 +57,60 @@ with open(DIRECTORY + DATASET, "r") as read_file:
         occupation_percentage = 1 - nbplaces / (nbplaces + nbvelos)
 
         # Ajouter les données à la liste principale
-        data_list.append({'mdate': mdate, 'occupation_percentage': occupation_percentage, 'etat': etat})
-
+        data_list.append({'mdate': mdate, 'y': occupation_percentage, 'etat': etat})
+        data_list_prophet.append({'ds': mdate, 'y': occupation_percentage})
+        
         # Convertir la date en format de jour uniquement
         day_only = mdate.date()
 
         # Ajouter les données à la liste par jour
-        data_list_per_day.append({'mdate': day_only, 'occupation_percentage': occupation_percentage, 'etat': etat})
+        data_list_per_day.append({'ds': day_only, 'y': occupation_percentage, 'etat': etat})
+        
 
 # Créer un DataFrame Pandas à partir de la liste principale
 df = pd.DataFrame(data_list)
+df_prophet = pd.DataFrame(data_list_prophet)
 #prophet_instance.fit(df)
 # Convertir la colonne 'mdate' en format de date
 df['mdate'] = pd.to_datetime(df['mdate'])
+df_prophet['ds'] = pd.to_datetime(df['mdate'])
 
 # Trier le DataFrame par date
-df = df.sort_values(by='mdate')
+df = df.loc[df['mdate'].apply(lambda x: x not in [pd.NaT])]
+df_prophet = df_prophet.loc[df_prophet['ds'].apply(lambda x: x not in [pd.NaT])]
+df = df.sort_values(by='mdate').iloc[47190: 47390]
+df_prophet = df_prophet.sort_values(by='ds')
+
+future = pd.DataFrame() 
+future['ds'] = df_prophet['ds'].apply(lambda x: (x + pd.DateOffset(years=1)).date())
+future = future.drop_duplicates()
+
+model = pr.Prophet()
+model.fit(df_prophet)
+forecast = model.predict(future) 
+#print(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].head())
 
 # Créer un DataFrame Pandas pour les données par jour
 df_per_day = pd.DataFrame(data_list_per_day)
-
 # Afficher le nombre de journées uniques
-num_unique_days = df_per_day['mdate'].nunique()
+num_unique_days = df_per_day['ds'].nunique()
 print("Number of unique days:", num_unique_days)
+aggregation_funtion = {'ds': 'first', 'y': 'mean', 'etat': 'first'}
+df_per_day = df_per_day.aggregate(aggregation_funtion)
+print(df_per_day['ds'])
 
 # Tacer les points avec etat=="DECONNECTEE" en rouge et les autres en bleu
 plt.figure(figsize=(10, 6))
 
 # Plotting points with etat=="DECONNECTEE" in red (case-insensitive) and smaller points
-plt.scatter(df[df['etat'].str.lower() == DECONNECTEE_VALUE.lower()]['mdate'], df[df['etat'].str.lower() == DECONNECTEE_VALUE.lower()]['occupation_percentage'], c='red', label=DECONNECTEE_VALUE, s=40)
+#plt.scatter(df[df['etat'].str.lower() == DECONNECTEE_VALUE.lower()]['mdate'], df[df['etat'].str.lower() == DECONNECTEE_VALUE.lower()]['y'], c='red', label=DECONNECTEE_VALUE, s=40)
 
 # Plotting other points in blue and smaller points
-plt.scatter(df[df['etat'].str.lower() != DECONNECTEE_VALUE.lower()]['mdate'], df[df['etat'].str.lower() != DECONNECTEE_VALUE.lower()]['occupation_percentage'], c='blue', label='Occupation Percentage', s=4)
+plt.scatter(df_per_day[df_per_day['etat'].str.lower() != DECONNECTEE_VALUE.lower()]['mdate'], df_per_day[df_per_day['etat'].str.lower() != DECONNECTEE_VALUE.lower()]['y'], c='blue', label='Occupation Percentage', s=4)
 
 # Connecting all points with a line
-plt.plot(df['mdate'], df['occupation_percentage'], color='black', linestyle='-', linewidth=0.5)
+plt.plot(df['mdate'], df['y'], color='black', linestyle='-', linewidth=0.5)
+#plt.plot(forecast['ds'], forecast['yhat'], color="blue", linestyle='-', linewidth=0.5)
 
 plt.title('Occupation Percentage Over Time')
 plt.xlabel('Date and Time')
